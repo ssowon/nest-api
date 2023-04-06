9a3e7460208472f7e54c61a2dfbbf24b8e0836d4
Sowon Choi <48286887+ssowon@users.noreply.github.com>
2023-04-07 00:06:13 +0900

    Feat: create, deleteOne 유닛 테스트 코드 작성
    * create, deleteOne Jest 유닛 테스트 코드 작성

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -36,6 +36,7 @@ describe('MoviesService', () => {
       expect(movie).toBeDefined();
       expect(movie.id).toEqual(1);
     });
+
     it('should throw 404 error', () => {
       try {
         service.getOne(999);
@@ -43,6 +44,41 @@ describe('MoviesService', () => {
         expect(e).toBeInstanceOf(NotFoundException);
         expect(e.message).toEqual('Movie with ID 999 not found.');
       }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const allMovies = service.getAll();
+      service.deleteOne(1);
+      const afterDelete = service.getAll();
+
+      expect(afterDelete.length).toBeLessThan(allMovies.length);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
     });
   });
 });
